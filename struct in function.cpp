7a6c2cfd8e2e72d7851477d03a7d04d76529f8ac
Mohan Prasath S <135501190+im-Mohan08@users.noreply.github.com>
2024-02-18 23:08:42 +0530

    struct in function.cpp
    updated

--- struct in function.cpp
+++ struct in function.cpp
@@ -1,32 +1,32 @@
 #include <stdio.h>
-#include <iostream>
+#include <iostream>         //header files c& c++
 using namespace std;
 
-struct rectangle
+struct rectangle                       //creating a structure
 {
-    int length;
+    int length;                 
     int breadth;
 };
-pair<int, int> intialize(struct rectangle *r, int l, int b)
+pair<int, int> intialize(struct rectangle *r, int l, int b)       //intialize value to struct by using function
 {
-    return make_pair(r->length = l, r->breadth = b);
+    return make_pair(r->length = l, r->breadth = b);               //make_pair is keyword which is use to return pair of value in a single function
 }
-int area(struct rectangle r)
+int area(struct rectangle r)                              // area-function
 {
     return r.length * r.breadth;
 }
-int change(struct rectangle *r, int l)
+int change(struct rectangle *r, int l)                  // change-function used to change the argument in a structure
 {
     return r->length = l;
 }
 int main()
 {
-    struct rectangle r;
-    pair<int, int> a = intialize(&r, 20, 12);
-    cout << a.first << endl
+    struct rectangle r;                                    // r is struct variable
+    pair<int, int> a = intialize(&r, 20, 12);              // pair intialisation using variable "a"
+    cout << a.first << endl                                // print the intialise value
          << a.second << endl;
-    cout << "Area of rectangle " << area(r) << endl;
-    cout << "change length " << change(&r, 11) << endl;
+    cout << "Area of rectangle " << area(r) << endl;            // here area function is called and it return /print the value
+    cout << "change length " << change(&r, 11) << endl;         // here change is the function which is used to change values of the structure
 
     return 0;
 }
